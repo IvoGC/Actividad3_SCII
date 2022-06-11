e7258a6ac2b0469c7b707be810d3d175701ca1bc
IvoGC <102243324+IvoGC@users.noreply.github.com>
2022-06-11 18:33:49 -0300

    Add files via upload

--- motor_discreto_integrador.m
+++ motor_discreto_integrador.m
@@ -46,7 +46,7 @@ rango=rank(M)
 %CONTROLADOR K e Ki
 %para el calculo del mismo se utiliza el metodo LQR para lo cual definimos
 %Q=[ia ; w; tita; Ki];
-Q=diag([1 1/1000 1 200]); R=20;
+Q=diag([1 1/1000 100 0.2]); R=2000;
 
 Ka=dlqr(Aa,Ba,Q,R);
 K_i=-Ka(4); K=Ka(1:3);
@@ -55,36 +55,36 @@ K_i=-Ka(4); K=Ka(1:3);
 X=zeros(3,round(tm/dt));
 X(:,1)=[0,0,0]; ref=pi; t=0;
 ia=X(1); tita=X(3) ; wr=X(2); Vh=tm/dt;h=dt;u=[];i=1;u_k(1)=0;
-Ve(1)=0;
+Ve(1)=0; Xf=[0 0 0];
 for ki=1:KMAX
     %t=[t ki*tm];
-    Ve(ki+1)=Ve(ki)+ref-C*X(:,ki);
-    u_k(ki)=-K*X(:,ki)+K_i*Ve(ki);
-    Y=C*X(:,ki);
+    Ve(ki+1)=Ve(ki)+ref-C*Xf';
+    u_k(ki)=-K*Xf'+K_i*Ve(ki);
+    Y=C*Xf';
     for kii=1:Vh
-        X_a=[X(1,i), X(2,i), X(3,i)] ; %X=[ia ; w ; tita ];
+        %X_a=[X(1,i), X(2,i), X(3,i)] ; %X=[ia ; w ; tita ];
+        X_a=X(:,i)';
         u(i)=u_k(ki);
         Xp_1=-RA/LAA*X_a(1)-(Km/LAA)*X_a(2)+(1/LAA)*u(i);   %ia_p
         Xp_2=(Ki_par/J)*X_a(1)-(Bm/J)*X_a(2);               %w_p (-1/J*TL(i))lo ignoramos momentaneamente
         Xp_3= X_a(2);                                       %tita_p
-                       
-    
+
         Xp_a=[Xp_1 , Xp_2 , Xp_3];
     
         Xf=X_a+ dt*Xp_a;
         X(:,i+1)=Xf;
         i=i+1;
     end
-    X(:,i-1)=Xf;
-    
 end
-u(i)=u_k(ki); t=0:h:KMAX*(Vh)*h;
+u(i)=u_k(ki); 
+t=0:h:KMAX*(Vh)*h;
+
 
 
 figure(1);hold on;
 subplot(2,2,1);plot(t,X(1,:),'r');grid on; title('Corriente ia');hold on;
-subplot(2,2,2);plot(t,X(2,:),'g');grid on;title('Ángulo tita');hold on;
-subplot(2,2,3);plot(t,X(3,:),'c');grid on;title('velocidad angular');hold on;
+subplot(2,2,2);plot(t,X(3,:),'g');grid on;title('Ángulo tita');hold on;
+subplot(2,2,3);plot(t,X(2,:),'c');grid on;title('velocidad angular');hold on;
 subplot(2,2,4);plot(t,u,'k');grid on;title('Acción de control');xlabel('Tiempo en Seg.');hold on;
 
 
