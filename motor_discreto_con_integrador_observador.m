837f471c8dd9b236a8955af8366ff2370c1931f4
IvoGC <102243324+IvoGC@users.noreply.github.com>
2022-06-15 20:10:20 -0300

    Add files via upload

--- motor_discreto_con_integrador_observador.m
+++ motor_discreto_con_integrador_observador.m
@@ -1,4 +1,4 @@
-clc; clear ; close all
+clc; clear 
 %{
 ---------------------------------------------------------------------------
 Implementar un sistema en variables de estado que controle el ángulo del motor, para
@@ -7,6 +7,25 @@ aparece sólo para pi/2, para –pi/2 es nulo. Hallar el valor de integración
 Euler adecuado.
 ---------------------------------------------------------------------------
 %}
+%implementacion de funciones a usar
+% PARAMETROS DE SIMULACION
+tm=1e-4; tf=2.6;
+%deltat=1*10^-5 ;
+KMAX=tf/tm;
+%Tt=tm*KMAX;
+dt=tm/10; 
+t=0:dt:tf;
+periodo=1;%[seg]
+torq=1.15*10^-3;
+Ref=pi/2*square(2*pi*t/periodo);%funcion de referencia que varia entre pi/2 y -pi/2
+TL=torq/2*square(2*pi*t/periodo)+torq/2;%Funcion torque que varia entre 0 y 1.15*10^-3
+Vh=tm/dt;u=[];i=1;  
+
+figure
+subplot(2,1,1);plot(t,Ref,'r');grid on; title('Corriente ia');hold on;
+subplot(2,1,2);plot(t,TL,'g');grid on;title('Ángulo tita');hold on;
+
+
 %DEFINO PARAMETROS
 LAA = 366*10^-6;
 J = 5*10^-9;
@@ -14,12 +33,6 @@ RA = 55.6;
 Bm = 0;
 Ki_par = 7.49*10^-3;
 Km = 7.53*10^-3;
-% PARAMETROS DE SIMULACION
-tm=1e-3; tf=2;
-%deltat=1*10^-5 ;
-dt=tm/10;
-KMAX=tf/dt;
-%Tt=tm*KMAX;
 
 %DEFINO MATRICES
 %X=[ia ; w ; tita ];
@@ -56,38 +69,34 @@ rango_o=rank(Mo)
 %para el calculo del mismo se utiliza el metodo LQR para lo cual definimos
 %Q=[ia ; w; tita; Ki];
 %Q=diag([1 1/1000 5000 0.2]); R=2000;
-Q=diag([1 1/100 500 0.2]); R=2000;
+Q=diag([100 1 500 0.2]); R=2000;
 Ka=dlqr(Aa,Ba,Q,R);
 K_i=-Ka(4); K=Ka(1:3);
 
 %Qo=diag([1 1 10]); Ro=[100 , 100 ; 1 , 1000];
-Qo=diag([1 1 10]); Ro=[100 , 1 ; 1 , 100];
+Qo=diag([1 1 10]); Ro=[10 , 1 ; 1 , 10];
 Ko=dlqr(Ao,Bo,Qo,Ro);
 
 
-%implementacion de funciones a usar
-Vh=tm/dt;u=[];i=1;  
-dt=tm/100;; t=0:dt:KMAX*(Vh)*dt;; periodo=1.5;%[seg]
-torq=1.15*10^-3;
-Ref=pi/2*square(2*pi*t/periodo);%funcion de referencia que varia entre pi/2 y -pi/2
-TL=torq/2*square(2*pi*t/periodo)+torq/2;%Funcion torque que varia entre 0 y 1.15*10^-3
 
-
+Referencia=pi/2;
 %ITERACION 
  
 Ve(1)=0; Xf=[0 0 0]; u_k(1)=0; Xhatf=[0;0 ;0]; Xhat_a=[0;0;0];
-
-X=zeros(3,round(tm/dt));    %X=[ia ; w ; tita ];
+Ref1(1)=0;
+X=zeros(3,round(tf/dt));    %X=[ia ; w ; tita ];
 X(:,1)=[0,0,0];     %condiciones iniciales de X
-ia=X(1); tita=X(3) ; wr=X(2);
-Xhat=zeros(3,round(tm/dt)); %Xhat=[iahat ; wrhat ; titahat ]
+
+Xhat=zeros(3,round(tf/dt)); %Xhat=[iahat ; wrhat ; titahat ]
 Xhat(:,1)=[0,0,0];  %condiciones iniciales de Xhat
-iahat=X(1); titahat=X(3) ; wrhat=X(2);
+Up(1)=0;
 for ki=1:KMAX
     %t=[t ki*tm];
-    Ve(ki+1)=Ve(ki)+Ref(ki)-C(2,:)*Xf';
-    %u_k(ki)=-K*Xf'+K_i*Ve(ki);
-    u_k(ki)=-K*Xhatf+K_i*Ve(ki);
+    %Ve(ki+1)=Ve(ki)+Ref(i)-C(2,:)*Xf';
+    Ve(ki+1)=Ve(ki)+Ref(i)-C(2,:)*Xf';
+    u_k(ki)=-K*Xf'+K_i*Ve(ki);
+    %u_k(ki)=-K*Xhatf+K_i*Ve(ki);
+    %u_k(ki)=-K(1:2)*Xf(1:2)'-K(3)*Xhatf(3)+K_i*Ve(ki);
     Y=C*Xf';
     Yhat=C*Xhatf;
     err=Y-Yhat;
@@ -97,14 +106,16 @@ for ki=1:KMAX
         
         X_a=X(:,i)';
         u(i)=u_k(ki);
+        
         Xp_1=-RA/LAA*X_a(1)-(Km/LAA)*X_a(2)+(1/LAA)*u(i);   %ia_p
-        Xp_2=(Ki_par/J)*X_a(1)-(Bm/J)*X_a(2)-1/J*TL(i);               %w_p (-1/J*TL(i))lo ignoramos momentaneamente
+        Xp_2=(Ki_par/J)*X_a(1)-(Bm/J)*X_a(2);               %w_p 
         Xp_3= X_a(2);                                       %tita_p
 
         Xp_a=[Xp_1 , Xp_2 , Xp_3];
         
         Xf=X_a+ dt*Xp_a;
         X(:,i+1)=Xf;
+        Ref1(i+1)=Ref(ki);
         %Xhat_a=Xhat(:,i)';
         Xhat_a=[Xhat(1,i) ; Xhat(2,i) ; Xhat(3,i)];
         Xhat_p=u(i)*B+Ko'*err+A*Xhat_a;
@@ -119,9 +130,12 @@ u(i)=u_k(ki);
 
 
 
-figure(1);hold on;
+figure(2);hold on;
 subplot(2,2,1);plot(t,X(1,:),'r');grid on; title('Corriente ia');hold on;
-subplot(2,2,2);plot(t,X(3,:),'g');grid on;title('Ángulo tita');hold on;
+subplot(2,2,2);plot(t,X(3,:),'r');grid on;title('Ángulo tita');hold on;
+plot(t,Ref);
 subplot(2,2,3);plot(t,X(2,:),'c');grid on;title('velocidad angular');hold on;
 subplot(2,2,4);plot(t,u,'k');grid on;title('Acción de control');xlabel('Tiempo en Seg.');hold on;
+% figure(3);hold on;
+% plot(t(1:length(u_k)),u_k,'k');grid on;title('Acción de control');xlabel('Tiempo en Seg.');hold on;
 
