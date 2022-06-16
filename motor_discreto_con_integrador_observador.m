8212ac0ec67f4770e172298d53147185fbdcca6d
IvoGC <102243324+IvoGC@users.noreply.github.com>
2022-06-16 19:48:24 -0300

    Add files via upload

--- motor_discreto_con_integrador_observador.m
+++ motor_discreto_con_integrador_observador.m
@@ -1,4 +1,4 @@
-clc; clear 
+clc; clear ;
 %{
 ---------------------------------------------------------------------------
 Implementar un sistema en variables de estado que controle el 嫕gulo del motor, para
@@ -9,7 +9,7 @@ Euler adecuado.
 %}
 %implementacion de funciones a usar
 % PARAMETROS DE SIMULACION
-tm=1e-4; tf=2.6;
+tm=1e-4; tf=2;
 %deltat=1*10^-5 ;
 KMAX=tf/tm;
 %Tt=tm*KMAX;
@@ -21,9 +21,9 @@ Ref=pi/2*square(2*pi*t/periodo);%funcion de referencia que varia entre pi/2 y -p
 TL=torq/2*square(2*pi*t/periodo)+torq/2;%Funcion torque que varia entre 0 y 1.15*10^-3
 Vh=tm/dt;u=[];i=1;  
 
-figure
-subplot(2,1,1);plot(t,Ref,'r');grid on; title('Corriente ia');hold on;
-subplot(2,1,2);plot(t,TL,'g');grid on;title('聲gulo tita');hold on;
+% figure
+% subplot(2,1,1);plot(t,Ref,'r');grid on; title('Referencia');hold on;
+% subplot(2,1,2);plot(t,TL,'g');grid on;title('Torque TL');hold on;
 
 
 %DEFINO PARAMETROS
@@ -69,73 +69,87 @@ rango_o=rank(Mo)
 %para el calculo del mismo se utiliza el metodo LQR para lo cual definimos
 %Q=[ia ; w; tita; Ki];
 %Q=diag([1 1/1000 5000 0.2]); R=2000;
-Q=diag([100 1 500 0.2]); R=2000;
+% Q=diag([1/0.01 1/1000 1 2]); R=200;
+Q=diag([1 1/1000 1 10]); R=1e2;
+
+%Q=diag([1000 1 900 20000]); R=20;
 Ka=dlqr(Aa,Ba,Q,R);
 K_i=-Ka(4); K=Ka(1:3);
 
 %Qo=diag([1 1 10]); Ro=[100 , 100 ; 1 , 1000];
-Qo=diag([1 1 10]); Ro=[10 , 1 ; 1 , 10];
+Qo=1e0*(diag([1 1 1])); Ro=1e3*([1 , 0 ; 0 , 1]);
 Ko=dlqr(Ao,Bo,Qo,Ro);
-
+%Ko=place(Ao,Bo,[0.999999 0.99998 0.99997]);
 
 
 Referencia=pi/2;
 %ITERACION 
  
-Ve(1)=0; Xf=[0 0 0]; u_k(1)=0; Xhatf=[0;0 ;0]; Xhat_a=[0;0;0];
+Ve=0; Xf=[0 0 0]; u_k(1)=0; 
 Ref1(1)=0;
 X=zeros(3,round(tf/dt));    %X=[ia ; w ; tita ];
-X(:,1)=[0,0,0];     %condiciones iniciales de X
-
-Xhat=zeros(3,round(tf/dt)); %Xhat=[iahat ; wrhat ; titahat ]
-Xhat(:,1)=[0,0,0];  %condiciones iniciales de Xhat
+X(:,1)=[0;0;0];     %condiciones iniciales de X
+err=0;
+x_int=[0 0 0];
+%Xhat=zeros(3,round(tf/dt)); %Xhat=[iahat ; wrhat ; titahat ]
+Xhat=[0;0;0];  %condiciones iniciales de Xhat
 Up(1)=0;
 for ki=1:KMAX
-    %t=[t ki*tm];
-    %Ve(ki+1)=Ve(ki)+Ref(i)-C(2,:)*Xf';
-    Ve(ki+1)=Ve(ki)+Ref(i)-C(2,:)*Xf';
-    u_k(ki)=-K*Xf'+K_i*Ve(ki);
-    %u_k(ki)=-K*Xhatf+K_i*Ve(ki);
-    %u_k(ki)=-K(1:2)*Xf(1:2)'-K(3)*Xhatf(3)+K_i*Ve(ki);
-    Y=C*Xf';
-    Yhat=C*Xhatf;
-    err=Y-Yhat;
+
+    u_k(ki)=-K*Xhat+K_i*Ve;
+    %u_k(ki)=-K*Xf'+K_i*Ve;
+    Y=C*x_int';
+    
+    zona_m=0.5;
+    if(u_k(ki)<=zona_m)
+       if((u_k(ki)>=-zona_m))
+        u_k(ki)=0;
+       else
+           u_k(ki)=u_k(ki)+zona_m;
+       end
+    else
+        u_k(ki)=u_k(ki)-zona_m;
+    end
+   sat=15;
+    if (u_k(ki)>=sat  )
+       u_k(ki)=sat;
+    end
+    if (u_k(ki)<=-sat)
+       u_k(ki)=-sat;
+    end
     
     for kii=1:Vh
         %X_a=[X(1,i), X(2,i), X(3,i)] ; %X=[ia ; w ; tita ];
-        
         X_a=X(:,i)';
         u(i)=u_k(ki);
         
+        %sistema original
         Xp_1=-RA/LAA*X_a(1)-(Km/LAA)*X_a(2)+(1/LAA)*u(i);   %ia_p
-        Xp_2=(Ki_par/J)*X_a(1)-(Bm/J)*X_a(2) -1/J*TL(i);               %w_p 
+        Xp_2=(Ki_par/J)*X_a(1)-(Bm/J)*X_a(2) -(1/J)*TL(i);    %w_p 
         Xp_3= X_a(2);                                       %tita_p
-
         Xp_a=[Xp_1 , Xp_2 , Xp_3];
-        
         Xf=X_a+ dt*Xp_a;
-        X(:,i+1)=Xf;
-        Ref1(i+1)=Ref(ki);
-        %Xhat_a=Xhat(:,i)';
-        Xhat_a=[Xhat(1,i) ; Xhat(2,i) ; Xhat(3,i)];
-        Xhat_p=u(i)*B+Ko'*err+A*Xhat_a;
-        Xhatf=Xhat_a + dt*Xhat_p;
-        Xhat(:,i+1)=[Xhatf(1) ; Xhatf(2); Xhatf(3)];         
+        X(:,i+1)=Xf;     
         i=i+1;
     end
-    %Xhat(:,i-1)=Xhatf; 
+    %observador
+    Yhat=C*Xhat;
+    err=Y-Yhat;
+    Xhat=u(i-1)*Bd+Ko'*err+Ad*Xhat;
+    Ve=Ve+Ref(i)-C(2,:)*x_int';
+    x_int=X(:,i)';
 end
 u(i)=u_k(ki); 
 
 
+%  figure(2);hold on;
+% subplot(2,2,1);plot(t,X(1,:),'g');grid on; title('Corriente ia');hold on;
+% subplot(2,2,2);plot(t,X(3,:),'r');grid on;title('聲gulo tita');hold on;
+% plot(t,Ref,'k');
+% subplot(2,2,3);plot(t,X(2,:),'c');grid on;title('velocidad angular');hold on;
+% subplot(2,2,4);plot(t,u);grid on;title('Acci鏮 de control');xlabel('Tiempo en Seg.');hold on;
 
-
-figure(2);hold on;
-subplot(2,2,1);plot(t,X(1,:),'r');grid on; title('Corriente ia');hold on;
-subplot(2,2,2);plot(t,X(3,:),'g');grid on;title('聲gulo tita');hold on;
-plot(t,Ref);
-subplot(2,2,3);plot(t,X(2,:),'c');grid on;title('velocidad angular');hold on;
-subplot(2,2,4);plot(t,u,'k');grid on;title('Acci鏮 de control');xlabel('Tiempo en Seg.');hold on;
-% figure(3);hold on;
-% plot(t(1:length(u_k)),u_k,'k');grid on;title('Acci鏮 de control');xlabel('Tiempo en Seg.');hold on;
-
+ figure(2);hold on;
+subplot(2,2,[1:2]);plot(t,X(3,:),'r');grid on;title('聲gulo tita');hold on;
+plot(t,Ref,'k');
+subplot(2,2,[3:4]);plot(t,u);grid on;title('Acci鏮 de control');xlabel('Tiempo en Seg.');hold on;
